12853022403dd84873cf78a1c4820994ef2c8db9
Billy Donahue <billy.donahue@gmail.com>
2020-01-20 13:19:57 -0500

    dev

--- trinketm0-meter/src/main.cpp
+++ trinketm0-meter/src/main.cpp
@@ -3,9 +3,13 @@
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_DotStar.h>
 
+static const float pi = 3.14159;
+
 
 Adafruit_SSD1306 display(128, 32, &Wire, -1);
 Adafruit_DotStar strip(1, INTERNAL_DS_DATA, INTERNAL_DS_CLK, DOTSTAR_BRG);
+
+int blink = 0;
 
 void setup() {
   pinMode(A0, INPUT);
@@ -19,14 +23,14 @@ void setup() {
   strip.show();  // Turn all LEDs off ASAP
 }
 
-int blink = 0;
-
 void loop() {
-  if (blink++ >= 10) {
-    digitalWrite(13, 1);
-    blink = 0;
-  } {
-    digitalWrite(13, 0);
+  {
+    uint32_t b = LOW;
+    if (++blink >= 10) {
+      b = HIGH;
+      blink = 0;
+    }
+    digitalWrite(13, b);
   }
 
   // put your main code here, to run repeatedly:
@@ -38,7 +42,7 @@ void loop() {
   int x = analogRead(A0);
   int y = analogRead(A3);
 
-  int xd = map(x, 0, 1023, 64 + 1, 64 + 32 - 1);
+  int xd = map(x, 0, 1023, 97, 127);
   int yd = map(y, 0, 1023, 31, 1);
   const int cross[9][2] = {
     {0, 0},
@@ -47,24 +51,18 @@ void loop() {
   };
   for (auto&& c : cross)
     display.drawPixel(xd + c[0], yd + c[1], SSD1306_WHITE);
-
-  display.drawRect(64, 0, 32, 32, SSD1306_WHITE);
-
-  float xf = x/512. - .5;
-  float yf = y/512. - .5;
+  display.drawRect(96, 0, 32, 32, SSD1306_WHITE);
+  float xf = (x - 512) / 512.;
+  float yf = (y - 512) / 512.;
   float r = hypotf(xf, yf);
   float th = atan2f(yf, xf);
-  float pi = 3.14159;
-
   if (th < 0)
     th = 2 * pi + th;
-
-  strip.setPixelColor(0, strip.ColorHSV(th * 65536. / (2*pi), r * 256, 128));
+  strip.setPixelColor(0, strip.ColorHSV(th * 65536. / (2*pi), min(255, (int)(r * 256)), 128));
   strip.show();
-
-  display.printf("xf=%3d\nyf=%3d\n", (int)(xf*100), (int)(yf*100));
-  display.printf("r=%3d\nth=%3d", (int)(r*100), (int)(th*180/pi));
+  display.printf("x,y=%d,%d\n", x, y);
+  display.printf("xf,yf=.%02d,.%02d\n", (int)(xf*100), (int)(yf*100));
+  display.printf("r,th=%d.%02d,%02d", (int)(r*100)/100, (int)(r*100)%100, (int)(th*180/pi));
   display.display();
-  
-  delay(50);
+  delay(100);
 }
