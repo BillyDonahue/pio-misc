90b469f7a55d37e603e1a3431c9349bc2eb15487
Billy Donahue <billy.donahue@gmail.com>
2020-01-21 13:53:10 -0500

    dev

--- espthing-blink/src/main.cpp
+++ espthing-blink/src/main.cpp
@@ -4,10 +4,16 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(13, OUTPUT);
 }
 
+bool blink = false;
+
 void loop() {
+  digitalWrite(13, blink ? HIGH : LOW);
+  blink = !blink;
   // put your main code here, to run repeatedly:
   Serial.printf("Hello, world! (%zu sec)\n", static_cast<size_t>(millis()));
   delay(1000);
+  
 }
